1889fc62d339dc1ee950b8398b6af38ccc3fb845
Gabriel Tan <gabtan99@gmail.com>
2021-04-14 18:42:58 +0800

    feat: setup of conversation history being preserved

--- main.py
+++ main.py
@@ -1,63 +1,91 @@
-import ray
-from ray import serve
 from fastapi import FastAPI
-import requests
-
+from fastapi.middleware.cors import CORSMiddleware
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from datetime import datetime
+
 import torch
-
 app = FastAPI()
-serve_handle = None
 
-@app.on_event("startup")  # Code to be run when the server starts.
-async def startup_event():
-    print("STARTING UP!@!@!@")
-    ray.init(address='auto') # Connect to the running Ray cluster.
-    client = serve.start()  # Start the Ray Serve instance.
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 
-    # Define a callable class to use for our Ray Serve backend.
-    class DialoGPT:
-        def __init__(self):
-            self.tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
-            self.model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-        def generate_response(tokenizer, model, chat_round, chat_history_ids, response):
-            """
-            Generate a response to some user input.
-            """
-            # Encode user input and End-of-String (EOS) token
-            new_input_ids = tokenizer.encode(response + tokenizer.eos_token, return_tensors='pt')
+# History 
+history_list = {}
 
-            # Append tokens to chat history
-            bot_input_ids = torch.cat([chat_history_ids, new_input_ids], dim=-1) if chat_round > 0 else new_input_ids
+MAX_TURNS = 5
+MODEL_LIST = {
+    1: {"name": "DialoGPT Small",
+        "tokenizer": AutoTokenizer.from_pretrained("microsoft/DialoGPT-small"), 
+        "model": AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")}
+}
 
-            # Generate response given maximum chat length history of 1250 tokens
-            chat_history_ids = model.generate(bot_input_ids, max_length=1250, pad_token_id=tokenizer.eos_token_id)
-        
-            reply =  tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+def generate_response(tokenizer, model, chat_round, chat_history_ids, query):
+    
+    # Encode user input and End-of-String (EOS) token
+    new_input_ids = tokenizer.encode(query + tokenizer.eos_token, return_tensors='pt')
 
-            print("Reply: ", reply)
-            # Return the chat history ids
-            return reply
+    # Append tokens to chat history
+    bot_input_ids = torch.cat([chat_history_ids, new_input_ids], dim=-1) if chat_round > 0 else new_input_ids
 
-        async def __call__(self, query):
-            chat_history_ids = None
-            print("HAHA2")
-            return generate_response(self.tokenizer, self.model, 0, chat_history_ids, "test",  query)
+    # Generate response given maximum chat length history of 1250 tokens
+    chat_history_ids = model.generate(bot_input_ids, max_length=1250, pad_token_id=tokenizer.eos_token_id)
 
-  
-    client.create_backend("DialoGPT", DialoGPT)
-    client.create_endpoint("generate", backend="DialoGPT")
+    reply = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)    
 
-    # Get a handle to our Ray Serve endpoint so we can query it in Python.
-    global serve_handle
-    serve_handle = serve.get_handle("generate")
+    #### for remembering the context
+    if chat_round == 0:
+        token = str(datetime.now().strftime("%Y%m%d%H%M%S"))
+        print("REGISTERING TOKEN")
+        history_list[token] = ({"chat_history_id": chat_history_ids,
+                            "chat_round": chat_round})
+    else:
+        print("ACCESSING TOKEN")
+        history_list[token] = ({"chat_history_id": chat_history_ids,
+                            "chat_round": chat_round + 1})
+    ####
 
+    return {"response": reply, "token": token}
 
 @app.get("/generate")
-async def generate(query):
-    return await serve_handle.remote(query)
+async def generate(query:str, token:str, model_id:int):
 
-@app.get("/")
+    model_set = MODEL_LIST.get(model_id)
+
+    # First time messaging
+    if token not in history_list:
+        return generate_response(model_set["tokenizer"], model_set["model"], 0, None, query)
+    else:
+        history = history_list.get(token)
+        return generate_response(model_set["tokenizer"], model_set["model"], history["chat_round"],  history["chat_history_id"], query)
+
+
+
+@app.get("/get_models")
+async def get_models():
+
+    lis = {}
+    for key in MODEL_LIST.keys():
+        lis[key] = MODEL_LIST.get(key)["name"]
+
+    return {"models": lis}
+
+    
+@app.post("/submit_rating")
+async def submit_rating(conversation, rating, model_id):
+
+    return {"response": 1}
+
+
+@app.get("/ping")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "pong"}
